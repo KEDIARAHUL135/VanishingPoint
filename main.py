455ead6529bd5495cf0be82fe22eec7ad5b5a76f
Rahul Kedia <kediarahul1011@gmail.com>
2020-09-26 15:01:51 +0530

    Add code to read input images

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+import os
 import cv2
 import numpy as np
 
 
+
+def ReadImage(InputImageFolderPath):
+    '''
+    This function will read all the images from the Folder path provided 
+    and store the images and their names in 2 lists and return it.
+    '''
+    Images = []                     # Input Images will be stored in this list.
+    ImageNames = []                 # Names of input images will be stored in this list.
+    
+    # Getting all image's name present inside the folder.
+    for ImageName in os.listdir(InputImageFolderPath):
+        InputImage = cv2.imread(InputImageFolderPath + "/" + ImageName)     # Reading images one by one.
+        Images.append(InputImage)                                           # Storing images.
+        ImageNames.append(ImageName)                                        # Storing image's names.
+    
+    return Images, ImageNames
+        
+
 if __name__ == "__main__":
-    print("Hello World!!")
-    pass
+    Images, ImageNames = ReadImage("InputImages")            # Reading all input images
+    
+    for i in Images:
+        cv2.imshow("dv", i)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
