a357a212ff141eb85e03f554605e499912449b4c
Rahul Kedia <kediarahul1011@gmail.com>
2020-09-26 16:50:53 +0530

    Add code to filter lines wrt their angle

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 import cv2
+import math
 import numpy as np
 
+
+# Threshold by which lines will be rejected wrt the horizontal
+REJECT_DEGREE_TH = 4.0
 
 
 def ReadImage(InputImageFolderPath):
@@ -21,6 +25,29 @@ def ReadImage(InputImageFolderPath):
     return Images, ImageNames
         
 
+def FilterLines(Lines):
+    FinalLines = []
+    
+    for Line in Lines:
+        [[x1, y1, x2, y2]] = Line
+
+        # Calculating equation of the line: y = mx + c
+        if x1 != x2:
+            m = (y2 - y1) / (x2 - x1)
+        else:
+            m = 100000000
+        c = y2 - m*x2
+        # theta will contain values between -90 -> +90. 
+        theta = abs(math.degrees(math.atan(m)))
+
+        # Rejecting lines of slope near to 0 degree or 90 degree and storing others
+        if REJECT_DEGREE_TH <= abs(theta) <= (90 - REJECT_DEGREE_TH):
+            FinalLines.append([x1, y1, x2, y2, m, c])
+
+    return FinalLines
+    
+
+
 def GetLines(Image):
     # Converting to grayscale
     GrayImage = cv2.cvtColor(Image, cv2.COLOR_BGR2GRAY)
@@ -32,13 +59,14 @@ def GetLines(Image):
     # Finding Lines in the image
     Lines = cv2.HoughLinesP(EdgeImage, 1, np.pi / 180, 50, 10, 10)
     
-    for Line in Lines:
-        [[x1, y1, x2, y2]] = Line
+    Lines = FilterLines(Lines)
+    
+    for line in Lines:
+        [x1, y1, x2, y2, m, c] = line
         cv2.line(Image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    cv2.imshow("Lines", Image)
-    cv2.waitKey(0)
-
+    
+    
 
 if __name__ == "__main__":
     Images, ImageNames = ReadImage("InputImages")            # Reading all input images
@@ -48,3 +76,6 @@ if __name__ == "__main__":
 
         # Getting the lines form the image
         GetLines(Image)
+
+        cv2.imshow("Ia", Image)
+        cv2.waitKey(0)
