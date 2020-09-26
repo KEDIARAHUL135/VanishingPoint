ab1c0287279ec9b8674eff8d479432d107ca11f1
Rahul Kedia <kediarahul1011@gmail.com>
2020-09-26 15:15:38 +0530

    Add code to get lines
    Lines are found with Probabilistic Hough Transform.

--- main.py
+++ main.py
@@ -21,10 +21,23 @@ def ReadImage(InputImageFolderPath):
     return Images, ImageNames
         
 
+def GetLines(Image):
+    # Converting to grayscale
+    GrayImage = cv2.cvtColor(Image, cv2.COLOR_BGR2GRAY)
+    # Blurring image to reduce noise.
+    BlurGrayImage = cv2.GaussianBlur(GrayImage, (5, 5), 1)
+    # Generating Edge image
+    EdgeImage = cv2.Canny(BlurGrayImage, 40, 255)
+
+    # Finding Lines in the image
+    Lines = cv2.HoughLinesP(EdgeImage, 1, np.pi / 180, 100)
+
+
 if __name__ == "__main__":
     Images, ImageNames = ReadImage("InputImages")            # Reading all input images
     
-    for i in Images:
-        cv2.imshow("dv", i)
-        cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    for i in range(len(Images)):
+        Image = Images[i]
+
+        # Getting the lines form the image
+        GetLines(Image)
