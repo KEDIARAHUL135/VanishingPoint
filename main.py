3c240a4d69d88915aba4cef018202088d8a8cffd
Rahul Kedia <kediarahul1011@gmail.com>
2020-09-27 21:46:45 +0530

    Add code to find vanishing point from lines

--- main.py
+++ main.py
@@ -74,6 +74,46 @@ def GetLines(Image):
     return Lines
     
 
+def GetVanishingPoint(Lines):
+    # We will apply RANSAC inspired algorithm for this. We will take combination 
+    # of 2 lines one by one, find their intersection point, and calculate the 
+    # total error(loss) of that point. Error of the point means root of sum of 
+    # squares of distance of that point from each line.
+    VanishingPoint = [None, None]
+    MinError = 100000000000
+
+    for i in range(len(Lines)):
+        for j in range(i+1, len(Lines)):
+            m1, c1 = Lines[i][4], Lines[i][5]
+            m2, c2 = Lines[j][4], Lines[j][5]
+
+            if m1 != m2:
+                x0 = (c1 - c2) / (m2 - m1)
+                y0 = m1 * x0 + c1
+
+                err = 0
+                for k in range(len(Lines)):
+                    m, c = Lines[k][4], Lines[k][5]
+                    m_ = (-1 / m)
+                    c_ = y0 - m_ * x0
+
+                    x_ = (c - c_) / (m_ - m)
+                    y_ = m_ * x_ + c_
+
+                    l = math.sqrt((y_ - y0)**2 + (x_ - x0)**2)
+
+                    err += l**2
+
+                err = math.sqrt(err)
+
+                if MinError > err:
+                    MinError = err
+                    VanishingPoint = [x0, y0]
+                
+    return VanishingPoint
+
+
+
 if __name__ == "__main__":
     Images, ImageNames = ReadImage("InputImages")            # Reading all input images
     
@@ -82,6 +122,11 @@ if __name__ == "__main__":
 
         # Getting the lines form the image
         Lines = GetLines(Image)
+
+        # Get vanishing point
+        VanishingPoint = GetVanishingPoint(Lines)
+
+        cv2.circle(Image, (int(VanishingPoint[0]), int(VanishingPoint[1])), 10, (0, 0, 255), -1)
 
         cv2.imshow("Ia", Image)
         cv2.waitKey(0)
